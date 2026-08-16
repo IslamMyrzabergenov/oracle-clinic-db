7ec7de235723962eaf787b8aba2ad24e850e0cc9
IslamMyrzabergenov <145836108+IslamMyrzabergenov@users.noreply.github.com>
2026-08-17 03:40:36 +0500

    Update 08_functions.sql

--- database/08_functions.sql
+++ database/08_functions.sql
@@ -1,1 +1,13 @@
+CREATE OR REPLACE FUNCTION get_total_revenue
+RETURN NUMBER
+AS
+    v_total NUMBER;
+BEGIN
+    SELECT NVL(SUM(amount), 0)
+    INTO v_total
+    FROM payments
+    WHERE status = 'PAID';
 
+    RETURN v_total;
+END;
+/

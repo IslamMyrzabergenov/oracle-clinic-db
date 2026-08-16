162271c771e01c595d81945bf0af27b3c7c65c7d
IslamMyrzabergenov <145836108+IslamMyrzabergenov@users.noreply.github.com>
2026-08-17 03:36:04 +0500

    Update 06_views.sql

--- database/06_views.sql
+++ database/06_views.sql
@@ -1,1 +1,16 @@
-
+CREATE OR REPLACE VIEW v_appointment_details AS
+SELECT
+    a.appointment_id,
+    p.first_name || ' ' || p.last_name AS patient_name,
+    d.first_name || ' ' || d.last_name AS doctor_name,
+    s.name AS specialization,
+    a.appointment_date,
+    a.status,
+    a.notes
+FROM appointments a
+JOIN patients p
+    ON a.patient_id = p.patient_id
+JOIN doctors d
+    ON a.doctor_id = d.doctor_id
+JOIN specializations s
+    ON d.specialization_id = s.specialization_id;

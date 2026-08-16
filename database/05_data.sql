3357680bb514e75f6254d71d62ec0ac3d675350f
IslamMyrzabergenov <145836108+IslamMyrzabergenov@users.noreply.github.com>
2026-08-17 03:20:33 +0500

    Update 05_data.sql

--- database/05_data.sql
+++ database/05_data.sql
@@ -1,1 +1,14 @@
+INSERT INTO specializations (name)
+VALUES ('Cardiologist');
 
+INSERT INTO specializations (name)
+VALUES ('Dentist');
+
+INSERT INTO specializations (name)
+VALUES ('Neurologist');
+
+INSERT INTO specializations (name)
+VALUES ('Therapist');
+
+INSERT INTO specializations (name)
+VALUES ('Surgeon');

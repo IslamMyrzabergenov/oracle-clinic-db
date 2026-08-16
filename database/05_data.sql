3194f3a542500f8aeb15aefaf836ad16434292b9
IslamMyrzabergenov <145836108+IslamMyrzabergenov@users.noreply.github.com>
2026-08-17 03:21:49 +0500

    Update 05_data.sql

--- database/05_data.sql
+++ database/05_data.sql
@@ -12,3 +12,18 @@ VALUES ('Therapist');
 
 INSERT INTO specializations (name)
 VALUES ('Surgeon');
+
+INSERT INTO doctors (first_name, last_name, specialization_id)
+VALUES ('John', 'Smith', 1);
+
+INSERT INTO doctors (first_name, last_name, specialization_id)
+VALUES ('Emily', 'Johnson', 2);
+
+INSERT INTO doctors (first_name, last_name, specialization_id)
+VALUES ('Michael', 'Brown', 3);
+
+INSERT INTO doctors (first_name, last_name, specialization_id)
+VALUES ('Sarah', 'Davis', 4);
+
+INSERT INTO doctors (first_name, last_name, specialization_id)
+VALUES ('David', 'Wilson', 5);

921da871808cd915ec9833c7074f224e24872f3b
IslamMyrzabergenov <145836108+IslamMyrzabergenov@users.noreply.github.com>
2026-08-17 03:40:53 +0500

    Update 08_functions.sql

--- database/08_functions.sql
+++ database/08_functions.sql
@@ -11,3 +11,31 @@ BEGIN
     RETURN v_total;
 END;
 /
+
+CREATE OR REPLACE FUNCTION get_patient_age (
+    p_patient_id IN NUMBER
+)
+RETURN NUMBER
+AS
+    v_birth_date patients.birth_date%TYPE;
+    v_age NUMBER;
+BEGIN
+    SELECT birth_date
+    INTO v_birth_date
+    FROM patients
+    WHERE patient_id = p_patient_id;
+
+    v_age := TRUNC(
+        MONTHS_BETWEEN(SYSDATE, v_birth_date) / 12
+    );
+
+    RETURN v_age;
+
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        RAISE_APPLICATION_ERROR(
+            -20003,
+            'Patient not found'
+        );
+END;
+/

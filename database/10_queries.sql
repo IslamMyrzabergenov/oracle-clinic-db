9d8349d4d95430cbff74fb44e2123973614a9972
IslamMyrzabergenov <145836108+IslamMyrzabergenov@users.noreply.github.com>
2026-08-17 03:43:09 +0500

    Update 10_queries.sql

--- database/10_queries.sql
+++ database/10_queries.sql
@@ -1,1 +1,15 @@
-
+SELECT
+    a.appointment_id,
+    p.first_name || ' ' || p.last_name AS patient_name,
+    d.first_name || ' ' || d.last_name AS doctor_name,
+    s.name AS specialization,
+    a.appointment_date,
+    a.status
+FROM appointments a
+JOIN patients p
+    ON a.patient_id = p.patient_id
+JOIN doctors d
+    ON a.doctor_id = d.doctor_id
+JOIN specializations s
+    ON d.specialization_id = s.specialization_id
+ORDER BY a.appointment_date;
